a929562a7d306c062b8bdc047883425df2f8f244
thanhdev <nguyenphucthanh1879@gmail.com>
2024-01-09 14:14:20 +0700

    add env

--- src/pages/admin/Setting/web/MailSetting/index.js
+++ src/pages/admin/Setting/web/MailSetting/index.js
@@ -51,24 +51,6 @@ function MailSetting() {
 
   const Update = async () => {
     try {
-      // // setLoading(true);
-      // const data = new FormData();
-      // data.append("file", file);
-      // data.append("upload_preset", "reading-time-storyboard");
-      // data.append("cloud_name", "df2s6srdu");
-      // data.append("folder", "reading-time-storyboard");
-
-      // const cloudinaryResponse = await fetch(
-      //   `https://api.cloudinary.com/v1_1/df2s6srdu/upload`,
-      //   {
-      //     method: "post",
-      //     body: data,
-      //   }
-      // );
-      // const cloudinaryData = await cloudinaryResponse.json();
-      // const imageUrl = await cloudinaryData.url;
-      // console.log(imageUrl);
-
       const res = await web.putMailSetting({
         Id: 1,
         Email_Sending_Address: sending
--- src/services/api/axiosConfig.js
+++ src/services/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import blockChainStorage from '~/utils/storage';
 
 const instance = axios.create({
-  baseURL: `http://localhost:8080`,
+  baseURL: process.env.REACT_APP_CLOUDINARY_SEVER_NAME,
 
   headers: {
     "Content-Type": "application/json",
